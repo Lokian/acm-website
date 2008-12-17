efa186dd86f4dd26e201a650cc2080b6518ce519
Robert Olson <rko618@gmail.com>
2008-12-16 19:25:51 -0700

    Added password reset stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resource :account, :controller => "users"
   map.resources :users
+  map.resources :password_resets
 
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new"
